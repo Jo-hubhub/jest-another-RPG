34552a97b87d3f891cfc54a46a0b260f297ad69c
Jo-hubhub <jbc66687@creighton.edu>
2022-02-06 13:21:25 -1000

    did action choices

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const Game = require('./lib/Game');
 
 new Game().initializeGame();
+    inquirer
+
+        .then(({ name }) => {
+            this.player = new Player(name);
+  
+            this.startNewBattle();
+        });
+
+Game.prototype.startNewBattle = function() {
+  if (this.player.agility > this.currentEnemy.agility) {
+    this.isPlayerTurn = true;
+  } else {
+    this.isPlayerTurn = false;
+  }
+};
 
